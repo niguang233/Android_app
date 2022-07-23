4cb98c4ea5be1e3948a4a5cb8f51a1605b2ef6a0
wangj <wangjunbo0806@yahoo.co.jp>
2022-07-23 22:11:06 +0900

    0723

--- app/src/main/java/to/msn/wings/widgetscheckbox/MainActivity.java
+++ app/src/main/java/to/msn/wings/widgetscheckbox/MainActivity.java
@@ -1,17 +1,26 @@
 package to.msn.wings.widgetscheckbox;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -19,6 +28,7 @@ import java.sql.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +37,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createSpinner();
+
+        final ArrayList<String> data = new ArrayList<>();
+        data.add("胡椒0");
+        data.add("胡椒1");
+        data.add("胡椒2");
+        data.add("胡椒3");
+        data.add("胡椒4");
+        data.add("胡椒5");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, data);
+        ListView list = findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+
+                                     @Override
+                                     public void onScrollStateChanged(AbsListView arg0, int arg1) {
+
+                                     }
+
+                                     @Override
+                                     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                                         if (firstVisibleItem + visibleItemCount + 3 > totalItemCount) {
+                                             adapter.add("新胡椒0");
+                                             adapter.add("新胡椒1");
+                                             adapter.add("新胡椒2");
+                                         }
+                                     }
+                                 });
+
+//        createSpinner();
 
 //        //Spinnerを取得
 //        Spinner sp = findViewById(R.id.spnOs);
@@ -89,33 +131,33 @@ public class MainActivity extends AppCompatActivity {
 //        );
     }
     //Spinnerに項目リストを登録するメソッド
-    private void createSpinner() {
-        ArrayList<String> list = new ArrayList<>();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd", Locale.JAPAN);
-        Calendar cal = Calendar.getInstance();
-
-        for (int i = 1; i < 11; i++) {
-            cal.set(Calendar.DAY_OF_MONTH, cal.get(Calendar.DAY_OF_MONTH) + 1);
-            list.add(format.format(cal.getTime()));
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item, list);
-        Spinner spn = findViewById(R.id.spnOs);
-        spn.setAdapter(adapter);
-        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Spinner sp = (Spinner) parent;
-                Toast.makeText(MainActivity.this,
-                        String.format("選択項目：%s", sp.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
+//    private void createSpinner() {
+//        ArrayList<String> list = new ArrayList<>();
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd", Locale.JAPAN);
+//        Calendar cal = Calendar.getInstance();
+//
+//        for (int i = 1; i < 11; i++) {
+//            cal.set(Calendar.DAY_OF_MONTH, cal.get(Calendar.DAY_OF_MONTH) + 1);
+//            list.add(format.format(cal.getTime()));
+//        }
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_spinner_dropdown_item, list);
+//        Spinner spn = findViewById(R.id.spnOs);
+//        spn.setAdapter(adapter);
+//        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Spinner sp = (Spinner) parent;
+//                Toast.makeText(MainActivity.this,
+//                        String.format("選択項目：%s", sp.getSelectedItem()),
+//                        Toast.LENGTH_SHORT).show();
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//    }
 }
